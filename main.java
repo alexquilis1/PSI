f34931887f6b846c7e3f30fe1553d494c35fe955
alexquilis1 <alexquilis1@gmail.com>
2022-05-24 11:43:09 +0200

    Added Look and Feel

--- main.java
+++ main.java
@@ -1,7 +1,16 @@
 import java.awt.EventQueue;
+import javax.swing.UIManager;
 public class main {
 
 	public static void main(String[] args) {
+
+		// Look and Feel
+		try {
+			UIManager.setLookAndFeel("com.jtattoo.plaf.mint.MintLookAndFeel");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
